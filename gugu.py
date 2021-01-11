6eccb951af44ab1a3a8c648ab38b87856a547159
Byeongchan Gwak <kbckbc@gmail.com>
2021-01-11 22:52:54 +0900

    print 수정

--- gugu.py
+++ gugu.py
@@ -1,5 +1,11 @@
-print('Which gugu ?',end=' ')
+print('Gugu ver 1.0\n')
 
-dan = int(input())
-for j in range(9):
-  print("{0:2d} * {1:2d} = {2:2d}".format(dan,j+1,dan*(j+1)))
+while True :
+    print('Which gugu? If you want to end, put 0 :',end=' ') 
+    
+    dan = int(input())
+    if dan == 0 :
+        break;
+    for j in range(9) :
+        print("{0:2d} * {1:2d} = {2:2d}".format(dan,j+1,dan*(j+1)))
+    print()
