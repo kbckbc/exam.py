7f6f7ab131fa7fde812d967cff83320aa0787257
Byeongchan Gwak <kbckbc@gmail.com>
2021-01-13 22:36:55 +0900

    split 추가

--- gugu.py
+++ gugu.py
@@ -1,11 +1,11 @@
 print('Gugu ver 1.0\n')
 
 while True :
-    print('Which gugu? If you want to end, put 0 :',end=' ') 
+    print('Which gugu? If you want to quit, put 0 :',end=' ') 
     
     dan = int(input())
     if dan == 0 :
         break;
     for j in range(9) :
         print("{0:2d} * {1:2d} = {2:2d}".format(dan,j+1,dan*(j+1)))
-    print()
+    print()# 한줄뛰우기
---
+++ split.py
@@ -0,0 +1,27 @@
+a='곽병찬,1,2|이연주,3,4|곽지성,5,6|곽채윤,7,8'
+print('\n원문 그대로')
+print("a:{0}".format(a))
+
+print('\n바깥쪽 리스트 split')
+b = a.split('|')
+print("b:{0}".format(b))
+
+for i in range(len(b)):
+    print("\nb[{0}]:{1}".format(i,b[i]))
+    c = b[i].split(',')
+    for j in range(len(c)):
+        print("     c[{0}]:{1}".format(j,c[j]),end=' ')
+
+print('\n변환전 갯수')
+print(len(b))
+print(len(b[0]))
+
+print('\n안쪽 리스트 split')
+for i in range(len(b)):
+    print("b[{0}]:{1}".format(i,b[i]))
+    b[i] = b[i].split(',')
+    print("b[{0}]:{1}".format(i,b[i]))
+
+print('\n변환후 갯수')
+print(len(b))
+print(len(b[0]))
