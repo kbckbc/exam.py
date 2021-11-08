cc595241bc1cc87a37d102b7ccc5a9f5255abe84
bychan <kbckbc@gmail.com>
2021-11-08 00:44:20 -0600

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python
 import time
 import serial
 
 ser = serial.Serial(
         port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
+        baudrate = 115200,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=1
 )
 counter=0
-
+cmd='CSE467 Embedded Computing Systems. bcgwak\n'
 while 1:
-        ser.write("Write counter: %d \n"%(counter))
+        ser.write(cmd.encode())
         time.sleep(1)
         counter += 1
